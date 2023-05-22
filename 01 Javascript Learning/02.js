442c3512b52272129c527db0552fc477404dcbbe
ladlamalik <ladlamalik.haxor@gmail.com>
2023-05-22 10:44:16 +0500

    first commit

---
+++ 01 Javascript Learning/02.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// intro to variable
+
+// variables can store some information
+// we can use that information later
+//we can change that information later
+
+// declare a variable
+
+var firstName = "Nadeem";
+
+// use a variablr
+console.log(firstName);
+
+// change value
+
+firstName = "Ehtisham";
+
+console.log(firstName);
