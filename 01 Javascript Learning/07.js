7f58a1c3220e0c2c9dba6fea507f05e428f2d923
ladlamalik <ladlamalik.haxor@gmail.com>
2023-06-03 18:49:45 +0500

    03rd june

---
+++ 01 Javascript Learning/07.js
@@ -0,0 +1,26 @@
+// trim()
+// toUpperCase()
+// toLowerCase()
+// slice()
+
+let firstName = "nadeem";
+
+// console.log(firstName.length);
+// firstName = firstName.trim();
+// console.log(firstName);
+// console.log(firstName.length);
+
+// console.log(firstName.length);
+// firstName = firstName.trim(); // "harshit"
+// console.log(firstName)
+// console.log(firstName.length);
+// console.log(firstName.toUpperCase())
+// console.log = firstName.toLowerCase();
+// start index
+// end index
+
+let newString = firstName.slice(0,4); // 
+console.log(newString);
+
+// let newString = firstName.slice(1); // hars
+// console.log(newString);
