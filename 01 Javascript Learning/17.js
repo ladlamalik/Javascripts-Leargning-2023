4e361c8a64117d84db5badd3ad1a33c0e219ec45
ladlamalik <ladlamalik.haxor@gamil.com>
2023-09-11 10:27:35 +0500

    11 sep

--- 01 Javascript Learning/17.js
+++ 01 Javascript Learning/17.js
@@ -1,2 +1,16 @@
 // nested if else
 
+// winning number 19
+
+// 19 your number is right
+// 17 too low
+// 20 too high
+
+let winningNumber = 19;
+let userGuess = +prompt("Guess a number")
+
+if(userGuess === winningNumber) {
+console.log("Your guess is right!!!") 
+}else{
+    console.log("Your guess is wrong!!")
+}
